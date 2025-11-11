1d41fcaf2714706337f4be0c448ac135b0497217
JeffSantos19 <jeff030891@gmail.com>
2025-11-11 11:16:38 -0300

    Add files via upload

---
+++ links.js
@@ -0,0 +1,12 @@
+const whatsappGroups = [
+  "https://chat.whatsapp.com/I3N4MxV2RIXCdYHqT80jGW",
+  "https://chat.whatsapp.com/DESjbETCjE3GxkHwrMmo1Q",
+  "https://chat.whatsapp.com/EzJgp04vtYtFCNIM6baPi1",
+  "https://chat.whatsapp.com/BCl4jg7CuEj1M1eTGmvers",
+  "https://chat.whatsapp.com/EkHCIqWipaL3PxyM4nROtK",
+  "https://chat.whatsapp.com/BVcGKKdUtvnIRa3zzYQJWz",
+  "https://chat.whatsapp.com/Dd4dXh8Em2d3hPhQtxRmw4",
+  "https://chat.whatsapp.com/HlzhJ3QEtGR4NioR15szty",
+  "https://chat.whatsapp.com/CEEmPm6dWcI6vhLtj0XhMk",
+  // Adicione quantos links de grupos do WhatsApp você quiser aqui
+];
